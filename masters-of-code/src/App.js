7244cc62b2f58c888825d47ae168f7f04bc1ab2a
An_pir <pirog.ariysk@gmail.com>
2025-02-06 13:58:05 +0500

    1

--- masters-of-code/src/App.js
+++ masters-of-code/src/App.js
@@ -1,9 +1,29 @@
-import './App.css';
+import "./App.css";
+import Costs from "./components/Costs/Costs";
 
 function App() {
+  const cost = [
+    {
+      date: new Date(2021, 2, 12),
+      description: "Холодильник",
+      amount: 999.99,
+    },
+    {
+      date: new Date(2021, 11, 25),
+      description: "MacBook",
+      amount: 1254.72,
+    },
+    {
+      date: new Date(2021, 3, 1),
+      description: "Джинсы",
+      amount: 49.99,
+    },
+  ];
+
   return (
     <>
-    <h1>Начнем изучение React</h1>
+      <h1>Начнем изучение React</h1>
+      <Costs cost={cost} />
     </>
   );
 }
