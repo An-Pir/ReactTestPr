8d4ce1f0b34100d989dfe45a9636e43ec2a671c6
Anatoly <an-pir@bk.ru>
2025-02-22 22:42:57 +0500

    1

--- stepik-react-course/input-test/src/Forms.jsx
+++ stepik-react-course/input-test/src/Forms.jsx
@@ -7,33 +7,32 @@ class Forms extends Component {
   };
 
   handleChange = (event) => {
-    this.setState({ [event.target.name]: event.target.value });
-  };
-  handleCheckboxChange = (event) => {
-    this.setState({ [event.target.name]: event.target.checked });
-  };
-  handleClick = () => {
-    this.setState();
-  };
-  handleForm = (event) => {
-    event.preventDefault();
-    this.validationName();
-    this.validationCheckBox();
-    this.setState({ email: "", subscription: false });
-  };
-  validationCheckBox = () => {
-    if (!this.state.subscription) {
-      alert("Подтвердите ваше согласие получать рассылку");
-    }
-  };
-  validationName = () => {
-    if (
-      !/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(this.state.email)
-    ) {
-      alert("Введите корректный Email !!!");
-    }
+    this.setState({ email: event.target.value });
   };
 
+  handleCheckboxChange = (event) => {
+    this.setState({ subscription: event.target.checked });
+  };
+
+  handleForm = (event) => {
+    event.preventDefault();
+  };
+
+  handleSubmit = () => {
+    const isValidEmail =
+      /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(
+        this.state.email.toLocaleLowerCase()
+      );
+    const isValidCheckBox = this.state.subscription;
+    if (!isValidEmail || !isValidCheckBox) {
+      alert("Заполните форму и согласие");
+      return;
+    }
+    this.setState({ email: "", subscription: false });
+
+    alert("Вы подписаны");
+  };
+    
   render() {
     const { email, subscription } = this.state;
     return (
@@ -48,16 +47,20 @@ class Forms extends Component {
           />
           Email
           <br />
-          <input
-            type="checkbox"
-            id="subscription"
-            name="subscription"
-            checked={subscription}
-            onChange={this.handleCheckboxChange}
-          />
-          <label htmlFor="subscription"> я хочу получать рассылку</label>
+          <label>
+            <input
+              type="checkbox"
+              id="subscription"
+              name="subscription"
+              checked={subscription}
+              onChange={this.handleCheckboxChange}
+            />
+            я хочу получать рассылку
+          </label>
           <br />
-          <button type="submit">Отправить</button>
+          <button type="submit" onClick={this.handleSubmit}>
+            Отправить
+          </button>
         </form>
       </div>
     );
