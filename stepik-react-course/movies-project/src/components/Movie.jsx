6dd8ee41b5349d7d08567c735d6abe1efa25f38a
An_pir <pirog.ariysk@gmail.com>
2025-02-26 12:03:22 +0500

    1

---
+++ stepik-react-course/movies-project/src/components/Movie.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+
+const Movie = (props) => {
+  const {
+    Title: title,
+    Year: year,
+    imdbID: id,
+    Type: type,
+    Poster: poster,
+  } = props;
+
+  return (
+    <div id={id} className="card movie">
+      <div classNameName="card-image waves-effect waves-block waves-light">
+        {poster === "N/A" ? (
+          <img
+            className="activator"
+            src={"https://placehold.jp/f0fff6/ffffff/150x300.png?text={title}"}
+          />
+        ) : (
+          <img className="activator" 
+          src={poster} />
+        )}
+      </div>
+      <div className="card-content">
+        <span className="card-title activator grey-text text-darken-4">
+          {title}
+        </span>
+        <p>
+          <a href="#">This is a link</a>
+        </p>
+        <p>
+          {year} <span>{type}</span>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Movie;
