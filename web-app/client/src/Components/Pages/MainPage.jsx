4ea2b5e2893513ff25c55254d17b7cfbeedd15c6
An_pir <pirog.ariysk@gmail.com>
2025-04-07 18:19:53 +0500

    мелкие редактирования

--- web-app/client/src/Components/Pages/MainPage.jsx
+++ web-app/client/src/Components/Pages/MainPage.jsx
@@ -3,15 +3,11 @@ import PageTitle from '../Common/PageTitle';
 
 const MainPage = () => {
   const navigate = useNavigate();
-  const handleClick = () => {
-    navigate('./login');
-  };
-
   return (
     <div className='flex-1 flex flex-col items-center justify-center p-4'>
       <PageTitle name='Кредитный калькулятор' />
       <p
-        onClick={handleClick}
+        onClick={() => navigate('./login') }
         className=' text-center text-lg font-bold   text-orange-400 underline cursor-pointer hover:text-orange-600 hover:text-xl  transition-all duration-500'
       >
         Для того чтобы воспользоваться кредитным калькулятором вам необходимо
